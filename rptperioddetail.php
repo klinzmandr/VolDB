0160a616e6c4be0a3f9d3792113e16754c2aaad7
klinzmandr <dave.klinzman@yahoo.com>
2014-12-03 12:03:25 -0800

    set default select date range to previous month

--- rptperioddetail.php
+++ rptperioddetail.php
@@ -14,8 +14,8 @@ session_start();
 include 'Incls/seccheck.inc';
 include 'Incls/datautils.inc';
 
-$sd = isset($_REQUEST['sd']) ? $_REQUEST['sd'] : date('Y-m-01', strtotime("this month"));
-$ed = isset($_REQUEST['ed']) ? $_REQUEST['ed'] : date('Y-m-t', strtotime('this month'));
+$sd = isset($_REQUEST['sd']) ? $_REQUEST['sd'] : date('Y-m-01', strtotime("previous month"));
+$ed = isset($_REQUEST['ed']) ? $_REQUEST['ed'] : date('Y-m-t', strtotime('previous month'));
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
 
 echo '<div class="container"><h3>Volunteer Service Detail&nbsp;&nbsp;<a class="btn btn-default" href="javascript:self.close();">CLOSE</a></h3>';
