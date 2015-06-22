09b2a6356dac0cb92d2c12a5e389e0cd97c74412
klinzmandr <dave.klinzman@yahoo.com>
2015-06-21 19:56:10 -0700

    add button to send to active/unlisted lists

--- sendchooser.php
+++ sendchooser.php
@@ -61,7 +61,10 @@ echo '<tr>
 <input type="submit" name="submit" value="Submit" onclick="return chkem()"></th>
 <th>List Name</th>';
 if ($_SESSION['VolSecLevel'] == 'voladmin')
- echo '<td><a class="btn btn-danger" href="sendemailvols.php?tokey[]=VolInactive">Send to Inactives</a></td>';
+ echo '<td>
+	<a class="btn btn-warning" href="sendemailvols.php?tokey[]=AUL">Send to Active/Unlisted</a> 
+ <a class="btn btn-danger" href="sendemailvols.php?tokey[]=VolInactive">Send to Inactives</a>
+ </td>';
 echo '</tr>';
 echo "
 <tr><td><input type=\"checkbox\" name=\"chkr\" 
