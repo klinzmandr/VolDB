ecc3d0f44087bc72b3b21305bd82928e7a0d4293
klinzmandr <dave.klinzman@yahoo.com>
2017-07-28 15:26:34 -0700

    updaed to include jquery invocations

--- PageTemplate.php
+++ PageTemplate.php
@@ -1,28 +1,47 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Template Title</title>
+<title>Page Title</title>
+<meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Bootstrap -->
-<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="css/bootstrap.min.css" rel="stylesheet" media="all">
 </head>
 <body>
 <script src="jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
-<!-- new comment line to test sync -->
+
+<script>
+// initial setup of jquery function(s) for page
+$(document).ready(function () {
+	alert(" example of action on document load");
+
+// this attaches an event to an object
+	$("h3").click(function () {
+    alert("example of a click of any header 3 like the page title"); 
+    });
+
+  });  // end ready function
+</script>
+
 <?php
 session_start();
 //include 'Incls/vardump.inc.php';
 //include 'Incls/seccheck.inc.php';
 //include 'Incls/mainmenu.inc.php';
-//include 'Incls/datautils.inc.php';
+include 'Incls/datautils.inc.php';
+
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
+
+if ($action == '') {
 
 print <<<pagePart1
-<h3>Template Heading</h3>
-<p>Page description ...</p>
-
+<h3>Page Title</h3>
+<p></p>
 
 pagePart1;
+
+}
 
 ?>
 
